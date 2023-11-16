215c8b8da84a06e819f271b1870953edf31c65c6
Miguel Monteiro <monteiro.miguel9752@gmail.com>
2023-11-15 22:57:54 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 from pytube import YouTube, Playlist
+def download_video(video_link:str, download_path: str): # recebe o link do vídeo e o caminho onde deseja baixar o arquivo(este segundo deve ser uma raw string-> r'caminho-do=download')
+    video = YouTube(video_link) # coleta as informações do vídeo
+    print(f"Title: {video.title}") # exibe o título do vídeo  
+    high_res_video = video.streams.get_highest_resolution() # pega o vídeo na maior resolução disponível
+    high_res_video.download(download_path) # baixa o vídeo no local desejado
 
-video = YouTube("video-link") #substitua pelo link do vídeo que deseja baixar
-print(f"Title: {video.title}")
-high_res_video = video.streams.get_highest_resolution()
-high_res_video.download(r'download-path') #substitua pelo caminho onde deseja baixar o arquivo
+def download_playlist(playlist_link: str, download_path: str): # recebe o link de uma playlist em vez de um único vídeo
+    playlist = Playlist(playlist_link) # coleta as informações da playlist
+    for playlist_video in playlist:
+        download_video(playlist_video, download_path) # faz o download de cada um dos vídeos da playlist
 
-
-playlist = Playlist('playlist-link') #substitua pelo link da playlist que deseja baixar
-for playlist_video in playlist:
-    playlist_video = YouTube(playlist_video).streams.get_highest_resolution()
-    print(f"Title: {playlist_video.title}")
-    playlist_video.download(r"donwload-path") #substitua pelo caminho onde deseja baixar os arquivos
-
+download_video("video-link"
+               , r"download-path")
+download_playlist("playlist-link"
+                  , r"download-path")
